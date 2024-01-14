79a5717f5fd7e5c741ff09812372f651328be0de
Tatyana Gerasina <gtstata@gmail.com>
2024-01-14 13:31:23 +0300

    Corrected Main

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int price = 20_225;
+        int price = 20_333;
         int rubleMile = 20;
-        int miles = service.calculate(20_225, 20); // должно получиться 500
+        int miles = service.calculate(20_333, 20);
         System.out.println(miles);
     }
 }
